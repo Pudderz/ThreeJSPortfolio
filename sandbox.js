d1fa2bb4c9a364f1e3f22e0ad2e70d82190b0347
Pudderz <mpudney6@gmail.com>
2020-11-02 20:33:36 +0000

    Rendered a model

--- sandbox.js
+++ sandbox.js
@@ -1,24 +1,19 @@
 
 const scene = new THREE.Scene();
+scene.background= new THREE.Color(0xdddddd);
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   1,
-  500
+  100
 );
 
 
 
 
-const cubeMaterials=[
-    new THREE.MeshLambertMaterial({map: new THREE.TextureLoader().load('images/elite.png'), side: THREE.FrontSide}),// Right side
-    new THREE.MeshPhongMaterial({color: 0xFFFFFF, side: THREE.DoubleSide}),// Left side
-    new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('images/elite.png'), side: THREE.DoubleSide}),// Top side
-    new THREE.MeshLambertMaterial({map: new THREE.TextureLoader().load('images/elite.png'), side: THREE.DoubleSide}),// Bottom side
-    new THREE.MeshLambertMaterial({map: new THREE.TextureLoader().load('images/elite.png'), side: THREE.DoubleSide}),// Front side
-    new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('images/elite.png'), side: THREE.DoubleSide}),// Back side
-]
 
+const hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 4);
+scene.add(hemiLight);
 
 
 // { alpha: true }
@@ -26,71 +21,62 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(2,2,2);
-const material = new THREE.MeshFaceMaterial(cubeMaterials)
+renderer.toneMapping = THREE.ReinhardToneMapping;
+renderer.toneMappingExposure = 2.3;
+renderer.shadowMap.enabled= true;
+const spotlight = new THREE.SpotLight(0xffa95c, 4);
+spotlight.castShadow = true;
+spotlight.shadow.bias= -0.0001;
+spotlight.shadow.mapSize.width= 1024*4;
+spotlight.shadow.mapSize.height= 1024*4;
+scene.add(spotlight);
 // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 
-const cube = new THREE.Mesh(geometry, material);
-camera.position.set(0,0,8)
+
+camera.position.set(0,2,5)
 camera.lookAt(0,0,0);
-   scene.add(cube);
 
 
-const materialLine= new THREE.LineBasicMaterial({color: 0x0000ff})
-const points = [];
-points.push( new THREE.Vector3( -1, 0, 0 ) );
- points.push( new THREE.Vector3( 0, 5, 0 ) );
- points.push( new THREE.Vector3( 1, 0, 0 ) );
 
-const lineGeometry = new THREE.BufferGeometry().setFromPoints( points );
-const line = new THREE.Line( lineGeometry, materialLine );
-scene.add(line);
+
 
 controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 
-//Ambient lighting
-// const ambientLight = new THREE.AmbientLight(0xFFFFFF, 1.5)
-// scene.add(ambientLight);
+ scene.add(new THREE.AxesHelper(500))
+let model
 
-// Point Light
-const pointLight1 = new THREE.PointLight(0xFF0040, 3, 50);
-scene.add(pointLight1)
-const pointLight2 = new THREE.PointLight(0x00FF00, 5, 50);
- scene.add(pointLight2)
-const pointLight3 = new THREE.PointLight(0x0000FF, 5, 50);
-scene.add(pointLight3)
-const pointLight4 = new THREE.PointLight(0x00FF00, 2, 50);
-scene.add(pointLight4)
-
-// const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 5)
-// directionalLight.position.set(0,50,50);
-// scene.add(directionalLight)
+new THREE.GLTFLoader().load('amongUs/scene.gltf', result=>{
+    model= result.scene.children[0];
+    model.position.set(0,0,0);
+    model.traverse(n =>{
+        if(n.isMesh){
+            n.castShadow = true;
+            n.receiveShadow = true;
+            if(n.material.map) n.material.map.anisotropy = 16;
+        }
+    })
+    scene.add(model);
+})
 
 
-// const spotlight = new THREE.Spotlight(0xFF45F6, 25)
-// spotlight.position.set(0,50,50);
-// scene.add(spotlight)
+
+
+
+
+
+
+
 const animate = function () {
   requestAnimationFrame(animate);
-  line.rotation.x += 0.01;
-  line.rotation.y += 0.01;
+
   const time = Date.now()*0.0005;
-//    directionalLight.position.x = Math.sin(time* 0.7)*30;
-//    directionalLight.position.y = Math.sin(time* 0.5)*30;
-//    directionalLight.position.z = Math.sin(time* 0.3)*30;
-  pointLight1.position.x = Math.sin(time* 0.7)*30;
-  pointLight1.position.y = Math.sin(time* 0.5)*30;
-  pointLight1.position.z = Math.sin(time* 0.3)*30;
-  pointLight2.position.x = Math.sin(time* 0.7)*-30;
-  pointLight2.position.y = Math.sin(time* 0.5)*-30;
-  pointLight2.position.z = Math.sin(time* 0.3)*-30;
-  pointLight3.position.x = Math.sin(time* 0.7)*20;
-  pointLight3.position.y = Math.sin(time* 0.5)*20;
-  pointLight3.position.z = Math.sin(time* 0.3)*20;
-   cube.rotation.x = Math.sin(time*0.02)*30;
-   cube.rotation.y = Math.sin(time*0,02)*30;
-   cube.rotation.z = Math.sin(time*0.02)*30;
+spotlight.position.set(
+    camera.position.x+10,
+    camera.position.y+10,
+    camera.position.z+10,
+)
+
 
 
   renderer.render(scene, camera);
@@ -108,3 +94,6 @@ window.addEventListener('resize', ()=>{
     camera.aspect = width/height;
     camera.updateProjectionMatrix()
 })
+
+
+
