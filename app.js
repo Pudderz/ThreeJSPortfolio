e30a6b882ccb1f9fabf1e175f5aa639c2b302ad7
Pudderz <mpudney6@gmail.com>
2020-11-03 17:02:07 +0000

    images are bent and transparent

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
-
-
-
-
 let speed = 0;
 let position = 0;
-let rounded= 0;
-let block = document.getElementById('block');
-let elems= [...document.querySelectorAll('.n')];
-let wrapper = document.querySelector('.wrap')
-window.addEventListener('wheel', e=>{
+let rounded = 0;
+let block = document.getElementById("block");
+let elems = [...document.querySelectorAll(".n")];
+let wrapper = document.querySelector(".wrap");
+window.addEventListener("wheel", (e) => {
+  speed += e.deltaY * 0.0003;
+});
 
-    speed+=e.deltaY*0.0003;
+var sketch = new Sketch();
 
-})
+let objs = Array(5).fill({ dist: 0 });
 
-let objs = Array(5).fill({dist:0})
-const raf=()=>{
-
-    position+=speed;
-     speed*=0.8;
-     rounded=Math.round(position);
-    objs.forEach((o,i)=>{
-        o.dist= Math.min(Math.abs(position-i),1)
-        o.dist= 1- o.dist**2;
-        // elems[i].style.transform = `scale(${1+0.4*o.dist})`
-        elems[i].style.transform = `scale(${1+0.4*o.dist})`
-    })
-    let diff = rounded - position;
-    position += Math.sign(diff)*Math.pow(Math.abs(diff),0.7)*0.05;
-    // console.log(position)
-    // block.style.transform =`translate(0,${position*100}px)`
-    wrapper.style.transform=`translate(0,${-position*100+50}px)`
-    window.requestAnimationFrame(raf)
-}
-
+const raf = () => {
+  position += speed;
+  speed *= 0.8;
+  rounded = Math.round(position);
+  objs.forEach((o, i) => {
+    o.dist = Math.min(Math.abs(position - i), 1);
+    o.dist = 1 - o.dist ** 2;
+     elems[i].style.transform = `scale(${1+0.4*o.dist})`
+    let scale = 1+0.2*o.dist;
+    sketch.meshes[i].position.y = i * 1.2 - position * 1.2;
+    sketch.meshes[i].scale.set(scale,scale,scale)
+    sketch.meshes[i].material.uniforms.distanceFromCenter.value = o.dist;
+  });
+  let diff = rounded - position;
+  position += Math.sign(diff) * Math.pow(Math.abs(diff), 0.7) * 0.05;
+  // console.log(position)
+  // block.style.transform =`translate(0,${position*100}px)`
+  wrapper.style.transform = `translate(0,${-position * 100 + 50}px)`;
+  console.log
+//   sketch.meshes.forEach((mesh, i) => {
+//     mesh.position.y = i * 1.2 + position * 1.2;
+    
+//   });
+  window.requestAnimationFrame(raf);
+  
+};
 
 raf();
--- templateApp.js
+++ templateApp.js
@@ -2,19 +2,26 @@
 
 function vertexShader() {
     return `
-    uniform float time;
+        uniform float time;
+        uniform float distanceFromCenter;
       varying vec2 vUv; 
       varying vec3 vPosition;
       varying vec2 pixels;
       float PI = 3.141592653589793238; 
       void main() {
-        vUv = uv; 
-        gl_Position = projectionMatrix * modelViewMatrix* vec4(position,1.0); 
+        vUv = (uv-vec2(0.5))*(0.8-0.2*distanceFromCenter*(2. - distanceFromCenter))+ vec2(0.5); 
+        vec3 pos = position;
+        pos.y+= sin(PI*uv.x)*0.01;
+        pos.y += sin(time*0.3)*0.02;
+        vUv.y += sin(time*0.3)*0.02;
+        gl_Position = projectionMatrix * modelViewMatrix* vec4(pos,1.0); 
       }
     `
   }
 function fragmentShader(){
     return `
+    uniform float time;
+    uniform float distanceFromCenter;
     uniform sampler2D texture1;
     uniform vec4 resolution;
     varying vec2 vUv;
@@ -22,10 +29,14 @@ function fragmentShader(){
     float PI = 3.141592653589793238;
     void main(){
         vec4 t = texture2D(texture1, vUv);
-        gl_FragColor = t;
+        float bw = (t.r + t.b + t.g )/3.;
+        vec4 another = vec4(bw,bw,bw,1);
+        gl_FragColor = mix(another,t,distanceFromCenter);
+        gl_FragColor.a = clamp(distanceFromCenter, 0.6, 1.);
     }
         
     `
+    
 }
 
 class Sketch{
@@ -40,6 +51,9 @@ class Sketch{
         this.scene = new THREE.Scene();
         this.addMesh();
         this.time= 0;
+        this.materials=[];
+        this.meshes=[];
+        this.groups=[];
         this.render()
         this.addResize()
         this.handleImages();
@@ -47,11 +61,16 @@ class Sketch{
 
 
     render(){
-        this.time++;
+        this.time+=0.05;
         // this.mesh.rotation.x += 0.01;
         // this.mesh.rotation.y += 0.02;
         this.camera.lookAt(0,0,0);
-
+        if(this.materials){
+            this.materials.forEach((mat)=>{
+            mat.uniforms.time.value = this.time;
+        })
+        }
+        
         this.camera.position.set(0,0,2)
         this.renderer.render( this.scene, this.camera );
         window.requestAnimationFrame(this.render.bind(this))
@@ -64,20 +83,20 @@ class Sketch{
             
             side: THREE.DoubleSide,
             uniforms:{
-                colorB: {type: 'vec3', value: new THREE.Color(0xACB6E5)},
-                colorA: {type: 'vec3', value: new THREE.Color(0x74ebd5)},
-                time:{type: 'f',value:0},
+                distanceFromCenter:{type:'f', value:0},
+                time:{type: 'f',value: 0},
                 texture1:{type:'t', value:null},
-                resolutionL: {type:'v4',value: new THREE.Vector4()},
+                resolution: {type:'v4',value: new THREE.Vector4()},
                 uvRate1:{
                     value: new THREE.Vector2(1,1)
                 },
             },
+            transparent:true,
             fragmentShader: fragmentShader(),
             vertexShader: vertexShader(),
         });
         this.mesh = new THREE.Mesh( this.geometry, this.material );
-	    this.scene.add( this.mesh );
+	    // this.scene.add( this.mesh );
     }
 
 
@@ -92,16 +111,30 @@ class Sketch{
         this.camera.updateProjectionMatrix()
     })}
     handleImages(){
+        
         let images = [...document.querySelectorAll('img')];
+       
       images.forEach((img, i)=>{
+        const group = new THREE.Group();
         let mat = this.material.clone();
         mat.uniforms.texture1.value = new THREE.Texture(img);
          mat.uniforms.texture1.value.needsUpdate = true;
-        // mat.uniforms.texture1.value = new THREE.Texture(img);
+        this.materials.push(mat);
+        // mat.wireframe= true;
         let geo = new THREE.PlaneBufferGeometry(1.5,1,20,20)
         let mesh = new THREE.Mesh(geo,mat)
-        this.scene.add(mesh)
-        mesh.position.y = i*1.2
+        
+        console.log(mesh);
+         group.add(mesh);
+         this.groups.push(group);
+         this.scene.add(group); 
+        
+         
+        this.meshes.push(mesh);
+        mesh.position.y = i*1.2;
+        group.rotation.y= -0.3;
+        group.rotation.x= -0.3;
+        group.rotation.z= -0.1;
       })
       }
       
@@ -109,7 +142,7 @@ class Sketch{
 
 }
 
-new Sketch();
+
 
 
 
