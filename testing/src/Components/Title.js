d5a9d304625483d059856f55675fae976c3f173b
Pudderz <mpudney6@gmail.com>
2020-11-05 21:09:48 +0000

    Added basic title, about button with updated styles

---
+++ testing/src/Components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Title() {
+    return (
+        <div className="top">
+            <h2 style={{color: 'white'}}>Matthew Pudney</h2>
+            <a style={{color: 'white'}} href="">About</a>
+        </div>
+    )
+}
