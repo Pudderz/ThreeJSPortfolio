7a9ac69af7589a57953fcf31eed5f7550605ecff
Pudderz <mpudney6@gmail.com>
2020-11-15 17:31:12 +0000

    Added layout to _app.js with router.pathname as path prop for transition key

--- nextjsversion/pages/_app.js
+++ nextjsversion/pages/_app.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/theme';
-import '../styles/App.css'
-import { GlobalProvider } from '../src/contexts/GlobalContext';
-import Test from '../components/canvas';
+import React, { useRef } from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../src/theme";
+import "../styles/App.css";
+import { GlobalProvider } from "../src/contexts/GlobalContext";
+// import Test from "../components/canvas";
+import { useRouter } from "next/router";
+import Layout from '../components/Layout'
+import Title from "../components/Title";
+import "../styles/App.scss"
+// import { Transition } from "react-transition-group";
 export default function MyApp(props) {
-  const { Component, pageProps } = props;
+  const { Component, pageProps, router } = props;
+  console.log(props)
+  const route = useRouter();
+  const tl = useRef();
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
@@ -21,15 +29,22 @@ export default function MyApp(props) {
   return (
     <React.Fragment>
       <Head>
-        <title>My page</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <title>Portfolio</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
         <GlobalProvider>
-          <Test/>
-          <Component {...pageProps} />
+          <Title path={router.pathname}/>
+          <Layout path={router.pathname}>
+                <Component
+                  {...pageProps}
+                  key={router.route}
+                />
+          </Layout>
+        
         </GlobalProvider>
       </ThemeProvider>
     </React.Fragment>
