0a81a88b96c014afc59bced67d3d7f377f852e57
Pudderz <mpudney6@gmail.com>
2020-11-16 10:36:07 +0000

    Fized issues where site was not resizing and going between normaly page and small page when resized

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,64 @@
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
-import  { PortfolioCanvas } from "../components/canvas";
+import React, { useEffect, useState, useRef } from "react";
+import { PortfolioCanvas } from "../components/canvas";
 import SmallFrontPage from "../components/smallFrontPage";
 import { HomeProvider } from "../src/contexts/HomeContext";
-import {getAllPostsForHome} from '../helpers/api'
-
+import { getAllPostsForHome } from "../helpers/api";
 
 export default function FrontPage(props) {
-  const [windowWidth, setWindowWidth] = useState();
-
-  const changeSize=(width)=>{
-    console.log(width)
-    console.log(`width is less than 1100 - ${width <= 1100}`)
-    if (width <= 1100 && windowWidth === "large") {
-      setWindowWidth("small");
-      console.log('small')
-      console.log(windowWidth)
-    } else if (width > 1100 && windowWidth === "small") {
-      setWindowWidth("large");
-      console.log('large')
-      console.log(windowWidth)
+  console.log("typeof window " + typeof window);
+  console.log(typeof window != "undefined");
+  const [windowWidth, setWindowWidth] = useState({
+    size:
+      typeof window != "undefined"
+        ? window.innerWidth < 900
+          ? "small"
+          : "large"
+        : "large",
+  });
+  const isSmall = useRef(false);
+  const changeSize = (width) => {
+    if (width <= 900 && isSmall.current === false) {
+      setWindowWidth({ size: "small" });
+      isSmall.current = true;
+      console.log("going to small");
+    } else if (width > 900 && isSmall.current === true) {
+      setWindowWidth({ size: "large" });
+      isSmall.current = false;
     }
-    console.log(windowWidth ==='large')
-  }
+  };
 
-  useEffect(()=>{
-    console.log('index mounted')
-    setWindowWidth(
-      window.innerWidth > 1100 ? "large" : "small"
-    );
+  useEffect(() => {
+    if (window.innerWidth <= 900) {
+      setWindowWidth({ size: "small" });
+      isSmall.current = true;
+    } else {
+      setWindowWidth({ size: "large" });
+      isSmall.current = false;
+    }
+    console.log("index mounted");
     window.addEventListener("resize", (e) => {
-     changeSize(e.target.innerWidth)
-     
+      changeSize(e.target.innerWidth);
     });
-    return()=>{
-      console.log('index unmounted')
+    return () => {
+      console.log("index unmounted");
       window.removeEventListener("resize", (e) => {
-        changeSize(e.target.innerWidth)
+        changeSize(e.target.innerWidth);
       });
-    }
-  },[])
+    };
+  }, []);
   return (
     <HomeProvider>
-         
-      <div>{windowWidth === "large" ? <PortfolioCanvas data={props.post}/> : <SmallFrontPage data={props.post} />}</div>
+      <div>
+        {windowWidth.size === "large" ? (
+          <PortfolioCanvas data={props.post} />
+        ) : (
+          <SmallFrontPage data={props.post} />
+        )}
+      </div>
     </HomeProvider>
   );
 }
-
 
 export async function getStaticProps() {
   const data = await getAllPostsForHome();
