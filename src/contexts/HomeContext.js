9e23d47b84c4c61a2d4e65547233a9d40e0e4345
Pudderz <mpudney6@gmail.com>
2020-11-16 18:31:52 +0000

    removed hard coded information from homecontext

--- src/contexts/HomeContext.js
+++ src/contexts/HomeContext.js
@@ -2,36 +2,7 @@ import React, { useState } from "react";
 
 export const HomeContext = React.createContext();
 
-const information = {
-  1: {
-    title: "Project 1",
-    descripttion: "Description 3",
-    primaryColor: "white",
-    secondaryColor: "#1b1f25",
-    textColor: "white",
-  },
-  0: {
-    title: "Project 2",
-    descripttion: "Description 2",
-    primaryColor: "#978d58",
-    secondaryColor: "#0b5269",
-    textColor: "white",
-  },
-  2: {
-    title: "Project 3",
-    descripttion: "Description 3",
-    primaryColor: "#FCBC3E",
-    secondaryColor: "#778899",
-    textColor: "white",
-  },
-  3: {
-    title: "Project 4",
-    descripttion: "Description 4",
-    primaryColor: "white",
-    secondaryColor: "#1e7753",
-    textColor: "white",
-  },
-};
+
 export const HomeProvider = ({ children }) => {
 //   const [attractTo, setAttractTo] = useState({ goTo: 0, shouldJump: false });
   const [attractMode, setAttractMode] = useState(false);
@@ -47,7 +18,6 @@ export const HomeProvider = ({ children }) => {
         setAttractMode: (boolean) => setAttractMode(boolean),
         displayNumber,
         attractMode,
-        information
       }}
     >
       {children}
