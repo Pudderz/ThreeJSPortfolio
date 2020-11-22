5320e0ef895fe35fd463db73761be98a482143a2
Pudderz <mpudney6@gmail.com>
2020-11-22 17:13:12 +0000

    added styling, success message and required attributes to form

--- components/form.js
+++ components/form.js
@@ -1,102 +1,193 @@
 import { Button } from "@material-ui/core";
-import React from "react";
-
+import React, { useState } from "react";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
+import { green } from "@material-ui/core/colors";
 const encode = (data) => {
   return Object.keys(data)
     .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
     .join("&");
 };
 
-export default class ContactForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      formUsername: "",
-      formEmail: "",
-      formMessage: "",
-    };
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: "relative",
+    width: "fit-content",
+    display:'inline-block',
+    marginLeft: 0,
+  },
+  buttonSuccess: {
+    backgroundColor: green[500],
+    "&:hover": {
+      backgroundColor: green[700],
+    },
+  },
+  fabProgress: {
+    color: green[500],
+    position: "absolute",
+    top: -6,
+    left: -6,
+    zIndex: 1,
+  },
+  buttonProgress: {
+    color: green[500],
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: -12,
+    marginLeft: -12,
+    "& svg": {
+      height: "100%",
+      width: "100%",
+      bottom: 0,
+    },
+  },
+  formInputs: {
+    fontFamily: "unset",
+    outline: "none",
+    padding: "1px",
+    fontSize: "16px",
+  },
+  successMessage:{
+    color: green[500],
+    display:'inline-block',
+    width:'fit-content',
+    paddingLeft:'30px',
   }
+}));
 
-  /* Here’s the juicy bit for posting the form submission */
+export default function ContactForm() {
+  const classes = useStyles();
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+  const [form, setForm] = React.useState({
+    formUsername: "",
+    formEmail: "",
+    formMessage: "",
+  });
 
-  handleSubmit = (e) => {
-    console.log(encode({ "form-name": "contact", ...this.state }));
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state }),
-    })
-      .then(() => alert("Success!"))
-      .catch((error) => alert(error));
-
+  const handleSubmit = (e) => {
     e.preventDefault();
+    if (!loading) {
+      setSuccess(false);
+      setLoading(true);
+
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", ...form }),
+      })
+        .then(() => {
+          // setSuccess(true);
+          // setLoading(false);
+          setTimeout(() => {
+            setSuccess(true);
+            setLoading(false);
+            setForm({
+              formUsername: "",
+              formEmail: "",
+              formMessage: "",
+            });
+          }, 500);
+        })
+        .catch((error) => {
+          console.error(error);
+          setLoading(false);
+        });
+    }
   };
 
-  handleChange = (e) => {
-    console.log(e.target.name) 
-    console.log(e.target.value) 
-    this.setState({ [e.target.name]: e.target.value },()=>console.log(this.state))};
+  const handleChange = (e) => {
+    if (!loading) {
+      if(success){
+        setSuccess(false);
+      }
+      setForm({ ...form, [e.target.name]: e.target.value });
+    }
+  };
 
-  render() {
-    const { formUsername, formEmail, formMessage } = this.state;
-    return (
+  return (
       <div className="form">
         <h2>Contact Form</h2>
 
         <hr />
         <form
-        onSubmit={this.handleSubmit}
-        data-netlify="true"
-        name="contact"
-        style={{ display: "flex", flexDirection: "column" }}
-      >
-        <input type="hidden" name="form-name" value="contact" />
-
-        <label htmlFor="name">Your Name:</label>
-        <input
-          id="name"
-          type="text"
-          name="formUsername"
-          value={formUsername}
-          onChange={this.handleChange}
-          onKeyDown={this.handleChange}
-          placeholder="Name"
-        />
-
-        <label htmlFor="email">Your Email:</label>
-        <input
-          id="email"
-          type="email"
-          name="formEmail"
-          value={formEmail}
-          onChange={this.handleChange}
-          onKeyDown={this.handleChange}
-          placeholder="Email"
-        />
-
-        <label htmlFor="message">Message:</label>
-        <textarea
-          id="message"
-          name="formMessage"
-          value={formMessage}
-          onChange={this.handleChange}
-          onKeyDown={this.handleChange}
-          style={{ resize: "vertical", minHeight: "75px", maxHeight: "500px" }}
-          placeholder="Message"
-        />
-
-        <Button
-          variant="contained"
-          color="default"
-          id="submitForm"
-          type="submit"
-          style={{ width: "fit-content"}}
+          onSubmit={handleSubmit}
+          data-netlify="true"
+          name="contact"
+          style={{ display: "flex", flexDirection: "column" }}
         >
-          Send
-        </Button>
-      </form>
+          <input type="hidden" name="form-name" value="contact" required />
+          
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            type="text"
+            name="formUsername"
+            value={form.formUsername}
+            onChange={handleChange}
+            onKeyDown={handleChange}
+            className={classes.formInputs}
+            placeholder="Name"
+            required
+          />
+
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            type="email"
+            name="formEmail"
+            value={form.formEmail}
+            onChange={handleChange}
+            onKeyDown={handleChange}
+            className={classes.formInputs}
+            placeholder="Email"
+            required
+          />
+
+          <label htmlFor="message">Message:</label>
+          <textarea
+            required
+            id="message"
+            name="formMessage"
+            value={form.formMessage}
+            className={classes.formInputs}
+            onChange={handleChange}
+            onKeyDown={handleChange}
+            style={{
+              resize: "vertical",
+              minHeight: "75px",
+              maxHeight: "500px",
+            }}
+            placeholder="Message"
+          />
+          <div className={classes.wrapper}>
+          
+            <Button
+              variant="contained"
+              color="default"
+              id="submitForm"
+              type="submit"
+              disabled={loading}
+              style={{ width: "fit-content" }}
+            >
+              Send
+            </Button>
+            {loading && (
+              <CircularProgress size={24} className={classes.buttonProgress} />
+            )}
+          {success && (
+                        <p className={classes.successMessage}>Message sent</p>
+                      )}
+          
+          </div>
+          
+        </form>
       </div>
-      
-    );
-  }
+  );
 }
